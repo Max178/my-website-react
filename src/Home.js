8854bb686ca3a4b64e99c526d918f6687782a21a
Max Pendse <maxpendse@Maxs-MacBook-Air-3.local>
2025-08-06 23:23:05 -0700

    Add some stuff

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import './Home.css';
+import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 function Home() {
   return (
     <div className="home-container">
       <h1>Home Page</h1>
-      <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      <img src="/IMG_4012.jpeg" alt="Site Visual" />
       <section>
         <h2>About Me</h2>
         <p>
           Hi, I'm a web developer passionate about building responsive and modern web applications.
         </p>
+      </section>
+      <section className="social-media">
+        <h2>Connect with Me</h2>
+        <div className="social-media-links">
+          <div className="social-link">
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
+              <FaFacebook size={32} />
+            </a>
+          </div>
+          <div className="social-link">
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+              <FaTwitter size={32} />
+            </a>
+          </div>
+          <div className="social-link">
+            <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+              <FaLinkedin size={32} />
+            </a>
+          </div>
+        </div>
       </section>
     </div>
   );
